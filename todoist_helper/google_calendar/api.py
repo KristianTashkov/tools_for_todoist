a9a7e22eaf5159af530a3ee833ee2b429953e109
Daniel Balchev <daniel@hyperscience.com>
2020-09-26 19:36:14 +0300

    Implement list_events wrapper

---
+++ todoist_helper/google_calendar/api.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+import warnings
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build                                                                                                                                               
+
+from todoist_helper.settings import CREDENTIALS_JSON_PATH, TOKEN_CACHE_PATH
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def _save_credentials(token):
+    if TOKEN_CACHE_PATH is None:
+        warnings.warn('TOKEN_CACHE_PATH is not set in settings.py')
+        return
+    with open(TOKEN_CACHE_PATH, 'wb') as token_io:
+        pickle.dump(token, token_io)
+    
+
+def _do_auth():
+    if TOKEN_CACHE_PATH is not None and os.path.exists(TOKEN_CACHE_PATH):
+        with open(TOKEN_CACHE_PATH, 'rb') as token_io:
+            token = pickle.load(token_io)
+        if token.valid:
+            return token
+        if token.expired and token.refresh_token:
+            token.refresh(Request())
+            _save_credentials(token)
+            return token
+
+    if CREDENTIALS_JSON_PATH is None:
+        raise ValueError('Set CREDENTIALS_JSON_PATH in settings.py to the path of the credentials.json file')
+    flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_JSON_PATH, SCOPES)
+    token = flow.run_local_server(port=0)
+    _save_credentials(token)
+    return token
+
+
+class CalendarApi:
+    def __init__(self):
+        self._token = None
+        self._calendar_service = None
+    
+    def auth(self):
+        if self._token is not None:
+            raise ValueError('CalendarApi object is already authenticated?')
+        self._token  = _do_auth()
+        self._calendar_service = build('calendar', 'v3', credentials=self._token)
+    
+    @property
+    def calendar_service(self):
+        if self._token is None:
+            raise ValueError('Call auth first')
+        return self._calendar_service
+
+    def iterate_events(self, calendar_id, **kwargs):
+        request = self.calendar_service.events().list(calendarId=calendar_id, **kwargs)
+        while request is not None:
+            response = request.execute()
+            for item in response['items']:
+                yield item
+            request = self.calendar_service.events().list_next(request, response)
