d10ed179eca6c2bf174c87934a3c8309ee403a54
Kristian Tashkov <kris.tashkov@hyperscience.com>
2025-01-19 14:45:40 +0200

    Add slack logging on server failures

--- tools_for_todoist/app.py
+++ tools_for_todoist/app.py
@@ -21,27 +21,27 @@ import logging
 import os
 import time
 
-import google.cloud.logging
-from google.auth.exceptions import DefaultCredentialsError
+import requests
 
 from tools_for_todoist.models.google_calendar import GoogleCalendar
 from tools_for_todoist.models.todoist import Todoist
 from tools_for_todoist.services.calendar_to_todoist import CalendarToTodoistService
 from tools_for_todoist.services.incentive_points import IncentivePoints
 from tools_for_todoist.services.night_owl_enabler import NightOwlEnabler
-from tools_for_todoist.storage import set_storage
+from tools_for_todoist.storage import KeyValueStorage, set_storage
 from tools_for_todoist.storage.storage import LocalKeyValueStorage, PostgresKeyValueStorage
 
 DEFAULT_STORAGE = os.path.join(os.path.dirname(__file__), 'storage', 'store.json')
 
 
-def setup_storage() -> None:
+def setup_storage() -> KeyValueStorage:
     database_config = os.environ.get('DATABASE_URL', None)
     if database_config is not None:
         storage = PostgresKeyValueStorage(database_config)
     else:
         storage = LocalKeyValueStorage(os.environ.get('FILE_STORE', DEFAULT_STORAGE))
     set_storage(storage)
+    return storage
 
 
 def setup_logger(logging_level=logging.DEBUG):
@@ -51,12 +51,6 @@ def setup_logger(logging_level=logging.DEBUG):
     formatter = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
-
-    try:
-        google_logging_client = google.cloud.logging.Client()
-        logger.addHandler(google_logging_client.get_default_handler())
-    except DefaultCredentialsError:
-        print('Logging not configured for Google Cloud.')
 
     logger = logging.getLogger('tools_for_todoist')
     logger.setLevel(logging_level)
@@ -78,29 +72,41 @@ def run_sync_service(logger):
         should_keep_syncing = True
         while should_keep_syncing:
             todoist_sync_result = todoist.sync()
-            should_keep_syncing = calendar_service.on_todoist_sync(todoist_sync_result)
+            should_keep_syncing = False
+            should_keep_syncing |= calendar_service.on_todoist_sync(todoist_sync_result)
             should_keep_syncing |= night_owl_enabler.on_todoist_sync(todoist_sync_result)
-            incentive_points.on_todoist_sync(todoist_sync_result)
+            should_keep_syncing |= incentive_points.on_todoist_sync(todoist_sync_result)
         time.sleep(10)
 
 
+def _send_slack_message(storage: KeyValueStorage, message: str) -> None:
+    slack_webhook_url = storage.get_value('logging.slack_webhook_url')
+    if slack_webhook_url is None:
+        return
+    payload = {'text': message, 'username': 'tools_for_todoist'}
+    requests.post(slack_webhook_url, json=payload)
+
+
 def main():
-    setup_storage()
+    storage = setup_storage()
     logger = setup_logger(os.environ.get('LOGGING_LEVEL', logging.DEBUG))
     retry_count = 0
     max_retries = 5
+    exception = None
     while retry_count < max_retries:
         try:
             run_sync_service(logger)
             retry_count = 0
-        except (KeyboardInterrupt, SystemExit):
-            raise
         except Exception as e:
+            exception = e
+            _send_slack_message(storage, f'TFT server restarting: {exception}')
             retry_count += 1
             if retry_count < max_retries:
                 logger.exception(
-                    f'Restarting app after exception! Retry {retry_count}.', exc_info=e
+                    f'Restarting app after exception! Retry {retry_count}.',
+                    exc_info=exception,
                 )
+    _send_slack_message(storage, f'TFT server crashed: {exception}')
 
 
 if __name__ == '__main__':
